49a68bdce75784e76ceb4bfa490fdc84d7ddeff0
soldesk <soldesk@DESKTOP-3D2JIQJ>
2023-04-07 10:04:42 +0900

    0407

---
+++ src/main/java/kr/co/softsoldesk/beans/TestBean2.java
@@ -0,0 +1,15 @@
+package kr.co.softsoldesk.beans;
+
+public class TestBean2 {
+
+	public void method1() {
+		System.out.println("beans.TestBean2.method1()");
+	}
+	
+	public int method2() {
+		int a1 = 100;
+		System.out.println("beans.TestBean2.method2()");
+		return a1;
+	}
+	
+}
